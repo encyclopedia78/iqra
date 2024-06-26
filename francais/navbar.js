bca04898273d0408bf672e2cbe5a6e99ad6a495a
Sara Qasbah <qasbah@hotmail.de>
2024-06-26 14:39:43 +0200

    adding verbs list

---
+++ francais/navbar.js
@@ -0,0 +1,16 @@
+// navbar.js
+
+document.addEventListener("DOMContentLoaded", function() {
+  const navbar = `
+      <div class="navbar">
+          <a href="../index.html">Home</a>
+          <a href="../francais.html">Francais</a>
+          <a href="../english.html">English</a>
+          <a href="../mathe.html">Mathe</a>
+          <a href="../about.html">Über Uns</a>
+          <a href="../services.html">Dienstleistungen</a>
+          <a href="../contact.html">Kontakt</a>
+      </div>
+  `;
+  document.getElementById("navbar").innerHTML = navbar;
+});
