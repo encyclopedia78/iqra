6c5283baaa97449f63c6f818f9237785616f1582
Sara Qasbah <qasbah@hotmail.de>
2025-02-03 15:03:50 +0100

    adding html template

---
+++ islam/Ritualrecht/navbar.js
@@ -0,0 +1,16 @@
+// navbar.js
+
+document.addEventListener("DOMContentLoaded", function() {
+  const navbar = `
+      <div class="navbar">
+          <a href="../../index.html">Home</a>
+          <a href="../../français.html">Français</a>
+          <a href="../../english.html">English</a>
+          <a href="../../mathe.html">Mathe</a>
+          <a href="../../programmieren.html">Programmieren</a>
+          <a href="../../islam.html">Islam</a>
+          <a href="../../العربية.html">العربية</a>
+      </div>
+  `;
+  document.getElementById("navbar").innerHTML = navbar;
+});
