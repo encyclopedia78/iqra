0124456faab53702f8d4c1c698b047258a2d7720
Sara Qasbah <qasbah@hotmail.de>
2025-02-04 08:34:01 +0100

    navbar fixed on the top

--- islam/navbar.js
+++ islam/navbar.js
@@ -1,16 +1,25 @@
 // navbar.js
 
-document.addEventListener("DOMContentLoaded", function() {
-  const navbar = `
-      <div class="navbar">
-          <a href="../index.html">Home</a>
-          <a href="../français.html">Français</a>
-          <a href="../english.html">English</a>
-          <a href="../mathe.html">Mathe</a>
-          <a href="../programmieren.html">Programmieren</a>
-          <a href="../islam.html">Islam</a>
-          <a href="../العربية.html">العربية</a>
-      </div>
-  `;
-  document.getElementById("navbar").innerHTML = navbar;
+// document.addEventListener("DOMContentLoaded", function() {
+//   const navbar = `
+//       <div class="navbar">
+//           <a href="../index.html">Home</a>
+//           <a href="../français.html">Français</a>
+//           <a href="../english.html">English</a>
+//           <a href="../mathe.html">Mathe</a>
+//           <a href="../programmieren.html">Programmieren</a>
+//           <a href="../islam.html">Islam</a>
+//           <a href="../العربية.html">العربية</a>
+//       </div>
+//   `;
+//   document.getElementById("navbar").innerHTML = navbar;
+// });
+
+document.addEventListener("scroll", function() {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+      navbar.style.padding = "5px 0"; // Verkleinert die Navbar beim Scrollen
+  } else {
+      navbar.style.padding = "10px 0";
+  }
 });
