63bb9a4c8ddf124193389f25431ce1601a1c16e7
Sara Qasbah <qasbah@hotmail.de>
2025-02-04 08:39:52 +0100

    bug fixing

--- islam/navbar.js
+++ islam/navbar.js
@@ -1,19 +1,19 @@
 // navbar.js
 
-// document.addEventListener("DOMContentLoaded", function() {
-//   const navbar = `
-//       <div class="navbar">
-//           <a href="../index.html">Home</a>
-//           <a href="../français.html">Français</a>
-//           <a href="../english.html">English</a>
-//           <a href="../mathe.html">Mathe</a>
-//           <a href="../programmieren.html">Programmieren</a>
-//           <a href="../islam.html">Islam</a>
-//           <a href="../العربية.html">العربية</a>
-//       </div>
-//   `;
-//   document.getElementById("navbar").innerHTML = navbar;
-// });
+document.addEventListener("DOMContentLoaded", function() {
+  const navbar = `
+      <div class="navbar">
+          <a href="../index.html">Home</a>
+          <a href="../français.html">Français</a>
+          <a href="../english.html">English</a>
+          <a href="../mathe.html">Mathe</a>
+          <a href="../programmieren.html">Programmieren</a>
+          <a href="../islam.html">Islam</a>
+          <a href="../العربية.html">العربية</a>
+      </div>
+  `;
+  document.getElementById("navbar").innerHTML = navbar;
+});
 
 document.addEventListener("scroll", function() {
   const navbar = document.querySelector(".navbar");
